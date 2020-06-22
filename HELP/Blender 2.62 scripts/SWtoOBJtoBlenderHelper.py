8576abcf7cb52bbac59bd8dea23f46dc42219557
Aeroanion <you@example.com>
2020-06-22 16:49:59 -0400

    Removed 'Blender 2.62 scripts' folder.

--- HELP/Blender 2.62 scripts/SWtoOBJtoBlenderHelper.py
+++
@@ -1,6 +0,0 @@
-# SolidWorks .OBJ Macro Version 2.0 ---> Blender 2.6
-# a small script to load .obj from Blendercommand line after export from SW.
-
-import bpy, sys
-
-bpy.ops.import_scene.obj(filepath=sys.argv[-1])
